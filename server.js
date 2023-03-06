ee180382ba99f9b840fe2c3e6cde65abdff61e88
Eddy Gonzalez <gon19005@byui.edu>
2023-03-06 02:43:39 -0700

    second project starting

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+const port = process.env.PORT || 3000
+
+
+app.use('/', require('./routes'));
+
+app.listen(port, () => {
+    console.log(`Server is running on poort ${port}`);
+
+});
