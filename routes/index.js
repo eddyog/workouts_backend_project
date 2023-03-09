7673bbf028715aaf03cad4e9aa795cfc4dafaca6
Eddy Gonzalez <gon19005@byui.edu>
2023-03-09 11:01:44 -0700

    a

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-    res.send('ooklklkl')
-})
 router.use('/', require('./swagger'));
 router.use('/workouts', require('./workouts'));
 
