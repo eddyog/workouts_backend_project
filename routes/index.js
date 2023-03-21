40ed524258ccd2fee892dafc4a0ba0d0cc2eb84d
Eddy Gonzalez <gon19005@byui.edu>
2023-03-21 10:00:51 -0600

    OAuth

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,14 @@ const router = express.Router();
 router.use('/', require('./swagger'));
 router.use('/workouts', require('./workouts'));
 
+const { requiresAuth } = require('express-openid-connect');
+
+
+const app = express();
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,31 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080
 const app = express();
 
+const {
+    auth,
+    requiresAuth
+} = require('express-openid-connect');
+
+app.use(
+    auth({
+        authRequired: false,
+        auth0Logout: true,
+        issuerBaseURL: process.env.ISSUER_BASE_URL,
+        baseURL: process.env.BASE_URL,
+        clientID: process.env.CLIENT_ID,
+        secret: process.env.SECRET,
+    })
+);
+
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+})
+
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
